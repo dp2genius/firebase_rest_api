9123a1165a2e510e962e80b89b1e0627347555f0
dp2genius <dp2genius@gmail.com>
2023-11-01 22:28:08 -0400

    MVP for read, archive and upload

--- functions/index.js
+++ functions/index.js
@@ -7,13 +7,83 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+const {onRequest} = require("firebase-functions/v2/https");
+const {Storage} = require("@google-cloud/storage");
+const logger = require("firebase-functions/logger");
+const archiver = require('archiver');
+// const cors = require("cors");
+const {PassThrough} = require('stream');
+
+const ProjectId = "vehiclehubdev";
+const BUCKET_NAME = "vehiclehubdev.appspot.com";
+
+const storage = new Storage({
+  projectId: ProjectId,
+});
+
+// dev
+const bucket = storage.bucket(BUCKET_NAME);
+console.log(bucket.getFiles().then(console.log));
+
+
+exports.helloWorld = onRequest(async (request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  console.log(request.body, request.query);
+
+  const files = ["avatar.jpg", "folder1/avatar.png"];
+  // const files = ["avatar.png"];
+  const destFilename = "my_archive.zip";
+
+  //
+
+  const bucket = storage.bucket(BUCKET_NAME);
+
+  // Where the zip file will be uploaded in firebase storage
+  const zipFile = bucket.file(destFilename);
+  const writeStream = zipFile.createWriteStream();
+
+  // Use passthrough stream as a middleman between archiver and cloud storage
+  const uploadStream = new PassThrough();
+
+  // Pipe the archive data to the file
+  uploadStream.pipe(writeStream);
+
+  // Initialize the archiver
+  const archive = archiver("zip", {
+    zlib: { level: 9 } // Sets the compression level.
+  });
+
+  archive.pipe(uploadStream);
+
+  files.forEach((filename) => {
+    const file = bucket.file(filename);
+    archive.append(file.createReadStream(), { name: filename });
+  });
+
+  archive.finalize();
+
+  writeStream.on("finish", () => {
+    // Start downloading
+
+    response.setHeader("Content-Disposition", `attachment; filename*=utf-8''${encodeURIComponent(fileName)}`);
+    const readStream = file.createReadStream();
+
+    readStream.on("error", (err) => {
+      console.error(err);
+      response.status(500).end();
+    });
+
+    readStream.pipe(response);
+  });
+
+});
+
+// exports.downloadZip = onRequest((request, response) => {
+//   const bucket = storage.bucket(BUCKET_NAME);
+
+//   const fileName = 'my_archive.zip';
+//   const file = bucket.file(fileName);
 // });
